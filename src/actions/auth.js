5c613945163b1b888b255143d28566104d98552f
byAlexks <alexanderksgamer@gmail.com>
2021-04-28 15:20:35 -0500

    login y logout hechos

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,65 +1,72 @@
-import { types } from "../types/types"
-import { firebase, googleAuthProvider } from "../firebase/firebase-config"
+import Swal from "sweetalert2";
+import { types } from "../types/types";
+import { firebase, googleAuthProvider } from "../firebase/firebase-config";
 import { finishLoading, startLoading } from "./ui";
 
-export const startLoginEmailPassword = (email, password) =>{
-    return (dispatch)=>{
-        dispatch(startLoading());
-        firebase.auth().signInWithEmailAndPassword(email, password)
-            .then( ({user})=>{
-                dispatch(finishLoading());
-                dispatch(
-                    login(user.uid, user.displayName)
-                );
-        } )
-        .catch(e=>{ console.log(e)
-            dispatch(finishLoading());
-        })
-    }
-}
-
-export const startRegisterWithEmailPasswordName = (email, password, name) => {
+export const startLoginEmailPassword = (email, password) => {
   return (dispatch) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then( async({user})=>{
-            await user.updateProfile({displayName: name})
-            dispatch(login(user.uid, user.displayName));
-        } )
+    dispatch(startLoading());
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        dispatch(finishLoading());
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((e) => {
+          console.log(e);
+          dispatch(finishLoading());
+          Swal.fire('Error', e.message, 'error') 
+      });
   };
 };
 
-
-export const startGoogleLogin = ()=>{
-    return (dispatch) =>{
-        firebase.auth().signInWithPopup(googleAuthProvider)
-            .then( ({user}) =>{
-                dispatch(
-                    login(user.uid, user.displayName)
-                )
-            })
-            .catch(e=>{
-                console.log(e);
-            })
-
-    }
-}
-
-export const login = (iud, displayName)=> ({
-    type: types.login,
-    payload:{
-        iud,
-        displayName
-    }  
-
-})
-
-export const starLogout = () => {
-  return async(dispatch) =>{
-    await firebase.auth().signOut();
-    dispatch( logout() );
-  }
+export const startRegisterWithEmailPasswordName = (email, password, name) => {
+  return (dispatch) => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async ({ user }) => {
+        await user.updateProfile({ displayName: name });
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((e)=>{
+          console.log(e);
+      });
+      
+  };
 };
 
-export const logout = () =>({
-    type: types.Logout
-})
+export const startGoogleLogin = () => {
+  return (dispatch) => {
+    firebase
+      .auth()
+      .signInWithPopup(googleAuthProvider)
+      .then(({ user }) => {
+        dispatch(login(user.uid, user.displayName));
+      })
+      .catch((e) => {
+        console.log(e);
+        Swal.fire('Error', e.message, 'error') 
+      });
+  };
+};
+
+export const login = (iud, displayName) => ({
+  type: types.login,
+  payload: {
+    iud,
+    displayName,
+  },
+});
+
+export const starLogout = () => {
+  return async (dispatch) => {
+    await firebase.auth().signOut();
+    dispatch(logout());
+  };
+};
+
+export const logout = () => ({
+  type: types.Logout,
+});
